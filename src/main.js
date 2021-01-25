d175cecc92f5dd0658d7d34884148de3361ea625
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-01-25 21:02:17 +0700

    Added Vuex, Created store.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store/store';
 
 import '@/assets/todo.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount('#app');
---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const LOCAL_STORAGE_KEY = 'todo-app-vue';
+export default new Vuex.Store({
+  state: {
+    todos: JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [
+      { text: 'Learn JavaScript ES6+ goodies', isDone: true },
+      { text: 'Learn Vue', isDone: false },
+      { text: 'Build something awesome', isDone: false },
+    ],
+  },
+  mutations: {
+  },
+});
