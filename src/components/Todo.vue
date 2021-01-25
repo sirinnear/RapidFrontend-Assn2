cfa187350a91d777b3c665d06c8a650c70149404
Sirin Chankao <sirin.cha@student.mahidol.edu>
2021-01-25 11:59:15 +0700

    Complete MD tutorial

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -7,34 +7,39 @@
         </header>
 
         <!-- This section should be hidden by default and shown when there are todos -->
-        <section class="main">
+        <section class="main" v-if="todos.length">
             <input id="toggle-all" class="toggle-all" type="checkbox">
             <label for="toggle-all">Mark all as complete</label>
             <ul class="todo-list">
                 <!-- These are here just to show the structure of the list items -->
                 <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-                <li class="completed">
+<!--                <li class="completed">-->
+<!--                    <div class="view">-->
+<!--                        <input class="toggle" type="checkbox" checked>-->
+<!--                        <label>Taste JavaScript</label>-->
+<!--                        <button class="destroy"></button>-->
+<!--                    </div>-->
+<!--                    <input class="edit" value="Create a TodoMVC template">-->
+<!--                </li>-->
+                <li v-for="todo in todos" :class="{ completed: todo.isDone, editing: todo === editing}">
                     <div class="view">
-                        <input class="toggle" type="checkbox" checked>
-                        <label>Taste JavaScript</label>
-                        <button class="destroy"></button>
+                        <input class="toggle" type="checkbox" v-model="todo.isDone">
+                        <label @dblclick="startEditing(todo)">{{ todo.text }}</label>
+                        <button class="destroy" @click="destroyTodo(todo)"></button>
                     </div>
-                    <input class="edit" value="Create a TodoMVC template">
-                </li>
-                <li v-for="todo in todos" :class="{ completed: todo.isDone }">
-                    <div class="view">
-                        <input class="toggle" type="checkbox">
-                        <label>{{ todo.text }}</label>
-                        <button class="destroy"></button>
-                    </div>
-                    <input class="edit" value="Rule the web">
+                    <input class="edit"
+                           @keyup.esc="cancelEditing"
+                           @keyup.enter="finishEditing"
+                           @blur="finishEditing"
+                           :value="todo.text"
+                    >
                 </li>
             </ul>
         </section>
         <!-- This footer should hidden by default and shown when there are todos -->
-        <footer class="footer">
+        <footer class="footer" v-if="todos.length">
             <!-- This should be `0 items left` by default -->
-            <span class="todo-count"><strong>0</strong> item left</span>
+            <span class="todo-count"><strong> {{ activeTodos.length }}</strong> item(s) left</span>
             <!-- Remove this if you don't implement routing -->
             <ul class="filters">
                 <li>
@@ -48,7 +53,7 @@
                 </li>
             </ul>
             <!-- Hidden if no completed items are left ↓ -->
-            <button class="clear-completed">Clear completed</button>
+            <button class="clear-completed" @click="clearCompleted()" v-show="completedTodos.length">Clear completed</button>
         </footer>
     </section>
     <footer class="info">
@@ -63,24 +68,62 @@
 </template>
 
 <script>
+    const LOCAL_STORAGE_KEY = 'todo-app-vue';
+
 export default {
   name: 'todo',
   data() {
       return {
         title: 'Todo Demo',
-          todos: [
+          todos: JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [
               { text: 'Learn JavaScript ES6+ goodies', isDone: true },
               { text: 'Learn Vue', isDone: false },
               { text: 'Build something awesome', isDone: false },
-          ]
+          ],
+          editing: null,
       }
   },
     methods: {
         createTodo(event) {
             const textbox = event.target;
-            this.todos.push({ text: textbox.value, isDone: false});
+            this.todos.push({ text: textbox.value.trim(), isDone: false});
             textbox.value = '';
+        },
+        startEditing(todo) {
+            this.editing = todo;
+        },
+        finishEditing(event) {
+            if (!this.editing) { return; }
+            const textbox = event.target;
+            this.editing.text = textbox.value.trim();
+            this.editing = null;
+        },
+        cancelEditing() {
+            this.editing = null;
+        },
+        destroyTodo(todo) {
+            const index = this.todos.indexOf(todo);
+            this.todos.splice(index, 1);
+        },
+        clearCompleted() {
+            this.todos = this.activeTodos;
         }
+    },
+    computed: {
+      activeTodos() {
+          return this.todos.filter(t => !t.isDone);
+      },
+      completedTodos() {
+          return this.todos.filter(t => t.isDone);
+      }
+    },
+    watch: {
+      todos: {
+          deep: true,
+          handler(newValue) {
+              localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newValue))
+          }
+      }
     }
 }
 </script>
